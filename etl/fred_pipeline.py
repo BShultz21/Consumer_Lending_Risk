299cbedf6a8e39d9389913995bd61dc9bd0492dc
Braden <braden.shultz21@gmail.com>
2026-03-09 20:17:33 -0400

    Created function to load to sql

--- etl/fred_pipeline.py
+++ etl/fred_pipeline.py
@@ -1,11 +1,13 @@
 import requests
 import json
 import os
-
-from pandas.core.interchange.from_dataframe import primitive_column_to_ndarray
+import psycopg
 
 
 def get_api_key():
+    """
+    This retrieves the api key from the project directory needed for a particular API
+    """
     script_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.dirname(script_dir)
     key_path = os.path.join(project_root, "config", "api_config.json")
@@ -28,24 +30,41 @@ class CallFredAPI:
         self.url = f'https://api.stlouisfed.org/fred/series/observations?series_id={self.series}&realtime_start=1991-01-01&api_key={self.key}&file_type=json'
 
     def get_historical_data(self, series):
+        """
+        This gets the historical data for a specific economic series from the FRED website
+        """
         self.set_series(series)
         self.set_url()
 
         response = requests.get(self.url)
         if response.status_code == 200:
-            self.parse_json_data(response.json())
+            data = [self.series]
+            parsed_json = self.parse_json_data(response.json())
+            data.append(parsed_json)
+            return data
         else:
             print(response.status_code)
 
     def parse_json_data(self, data):
+        """
+        This takes the data from the response from the FRED api call and parses the JSON to get the relevant information
+        """
         parsed_data = {}
-        if self.series in ['DRCCLACBS', 'CPIAUCSL']:
+        if self.series in ['DRCCLACBS', 'CPIAUCSL', 'UNRATE', 'MORTGAGE30US', 'TOTALSL']:
             for element in data['observations']:
-                data[element['date']] = element['value']
+                parsed_data[element['date']] = element['value']
 
-        print(data)
+        return parsed_data
+
+def load_data_to_sql(table, data):
+    with psycopg.connect("user=postgres") as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                f"SELECT column_name FROM information.schema.columns WHERE table_name = {table}")
+
 
 if __name__ == '__main__':
     api_call = CallFredAPI(get_api_key())
-    api_call.get_historical_data('CPIAUCSL')
+    print(api_call.get_historical_data('DRCCLACBS'))
+    print(load_data_to_sql("consumer_lending_risk", 0))
 
