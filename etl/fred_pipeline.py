f765fa958a9d26c9699828429195daa769a225a7
Braden <braden.shultz21@gmail.com>
2026-03-10 18:56:39 -0400

    Finished functionality to load historical data to SQL

--- etl/fred_pipeline.py
+++ etl/fred_pipeline.py
@@ -21,13 +21,13 @@ class CallFredAPI:
     def __init__(self, key):
         self.key = key
         self.series = None
-        self.url = f'https://api.stlouisfed.org/fred/series/observations?series_id={self.series}&realtime_start=1991-01-01&api_key={self.key}&file_type=json'
+        self.url = f'https://api.stlouisfed.org/fred/series/observations?series_id={self.series}&observation_start=1991-01-01&api_key={self.key}&file_type=json'
 
     def set_series(self, series):
         self.series = series
 
     def set_url(self):
-        self.url = f'https://api.stlouisfed.org/fred/series/observations?series_id={self.series}&realtime_start=1991-01-01&api_key={self.key}&file_type=json'
+        self.url = f'https://api.stlouisfed.org/fred/series/observations?series_id={self.series}&observation_start=1991-01-01&api_key={self.key}&file_type=json'
 
     def get_historical_data(self, series):
         """
@@ -52,19 +52,39 @@ class CallFredAPI:
         parsed_data = {}
         if self.series in ['DRCCLACBS', 'CPIAUCSL', 'UNRATE', 'MORTGAGE30US', 'TOTALSL']:
             for element in data['observations']:
-                parsed_data[element['date']] = element['value']
-
+                parsed_data[element['date'] ] = element['value']
+        print(parsed_data)
         return parsed_data
 
-def load_data_to_sql(table, data):
+def load_to_sql(table, data):
     with psycopg.connect("user=postgres") as conn:
         with conn.cursor() as cur:
-            cur.execute(
-                f"SELECT column_name FROM information.schema.columns WHERE table_name = {table}")
+            rows = []
+            indicator = data[0]
+            for date, value in data[1].items():
+                if value in ('', '.', 'N/A', None):
+                    value = None
+                rows.append((date, value, indicator))
+
+            cur.executemany(
+                f'INSERT INTO {table} (date, value, indicator_id) VALUES (%s, %s, %s)',
+                rows
+            )
+
+    print("Data has been loaded")
+
+
+def load_historical_data():
+    """
+    This calls the API to get historical data, parses the data, and then loads to SQL
+    """
+
+    api_call = CallFredAPI(get_api_key())
+    metrics = ['DRCCLACBS', 'CPIAUCSL', 'UNRATE', 'MORTGAGE30US', 'TOTALSL']
+    for metric in metrics:
+        data = api_call.get_historical_data(metric)
+        load_to_sql("consumer_lending_risk", data)
 
 
 if __name__ == '__main__':
-    api_call = CallFredAPI(get_api_key())
-    print(api_call.get_historical_data('DRCCLACBS'))
-    print(load_data_to_sql("consumer_lending_risk", 0))
-
+    load_historical_data()
