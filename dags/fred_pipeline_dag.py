1ddb9685291c58c3b05d05fdbeca909ea5f97ad3
Braden <braden.shultz21@gmail.com>
2026-03-16 20:44:14 -0400

    Created first step in airflow dag

---
+++ dags/fred_pipeline_dag.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from etl.fred_pipeline import load_api_data
+
+with DAG(
+        dag_id="consumer_lending_risk",
+        start_date=datetime(2026, 3, 12),
+        schedule="@weekly",
+        catchup=False
+):
+    """
+    load_fred_data = PythonOperator(
+        task_id="load_fred_data",
+        python_callable= load_api_data,
+    )
+    """
+    truncate_sql_table = SQLExecuteQueryOperator(
+        task_id="truncate_sql_table",
+        sql="TRUNCATE TABLE consumer_lending_risk;",
+        conn_id="postgres",
+    )
